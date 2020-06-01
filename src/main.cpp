3815b0cc9e10bacb85c98c81385c96550f3174ad
Keith Leonardo <kleonardo52@gmail.com>
2020-06-01 19:38:05 +1000

    Add toggles for fullscreen mode and clear color

--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,6 @@ int main(int argc, char **argv) {
     SDL_GetDesktopDisplayMode(0, &native_res);
 
     auto flags = SDL_WINDOW_VULKAN | SDL_WINDOW_RESIZABLE;
-    if(fullscreen) {
-        flags |= SDL_WINDOW_FULLSCREEN;
-    }
     int width = fullscreen ? native_res.w : 640;
     int height = fullscreen ? native_res.h : 480;
 
@@ -39,8 +36,29 @@ int main(int argc, char **argv) {
             if(e.type == SDL_QUIT) {
                 running = false;
             }
+            if(e.type == SDL_MOUSEBUTTONDOWN) {
+                if(e.button.button == SDL_BUTTON_LEFT) {
+                    renderer.set_fill(255, 255, 255, 255);
+                }
+                else {
+                    renderer.set_fill(0, 0, 0, 0);
+                }
+            }
             if(e.type == SDL_KEYDOWN) {
-                renderer.set_fill(255, 255, 255, 255);
+                if(e.key.keysym.sym == SDLK_F9) {
+                    if(!fullscreen)  {
+                        SDL_SetWindowFullscreen(
+                            window, SDL_WINDOW_FULLSCREEN
+                        );
+                        fullscreen = true;                        
+                    }
+                    else {
+                        SDL_SetWindowFullscreen(
+                            window, 0
+                        );
+                        fullscreen = false;
+                    }
+                }
             }
         }
     }
